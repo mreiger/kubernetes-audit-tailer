6ed13f25f15f0d7f3b5b49ce73db733b59ba949e
iz00242 <michael.reiger@f-i-ts.de>
2020-04-24 18:52:59 +0200

    Convert to string

--- pkg/controllers/audit/webhook.go
+++ pkg/controllers/audit/webhook.go
@@ -28,8 +28,8 @@ func NewController(logger *zap.SugaredLogger, client hec.HEC) *Controller {
 
 // AuditEvent handles an audit event
 func (c *Controller) AuditEvent(response http.ResponseWriter, request *http.Request) {
-	BodyStringBase64, _ := ioutil.ReadAll(request.Body)
-	c.logger.Infow("received audit event", "request", BodyStringBase64)
+	Body, _ := ioutil.ReadAll(request.Body)
+	c.logger.Infow("received audit event", "request", string(Body))
 	// BodyString, err := base64.URLEncoding.DecodeString(string(BodyStringBase64))
 	// if err != nil {
 	// 	c.logger.Errorw("error base64 decoding the body", "error", err)
@@ -38,13 +38,18 @@ func (c *Controller) AuditEvent(response http.ResponseWriter, request *http.Requ
 	// }
 	// c.logger.Infow("received audit event", "request base64decoded", BodyString)
 
-	event := hec.NewEvent(BodyStringBase64)
+	event := hec.NewEvent(Body)
 	event.SetHost("HOST")
 	event.SetTime(time.Now())
 	event.SetSource("SOURCE")
 	event.SetSourceType("SOURCETYPE")
 
-	c.logger.Infow("HEC Event", event.Event)
+	c.logger.Infow("HEC Event",
+		"Host", event.Host,
+		"Time", event.Time,
+		"Source", event.Source,
+		"Sourcetype", event.SourceType,
+	)
 
 	err := c.client.WriteEvent(event)
 	if err != nil {
